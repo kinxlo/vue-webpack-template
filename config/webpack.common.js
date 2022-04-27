412052dea461ec5d2fd59b7ce7a3496c7c06befe
Kingsley Solomon <kinxly@gmail.com>
2022-04-27 23:25:21 +0100

    [before adding vuetify]

---
+++ config/webpack.common.js
@@ -0,0 +1,79 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const paths = require('./paths')
+
+module.exports = {
+  // Where webpack looks to start building the bundle
+  entry: [paths.src + '/main.js'],
+
+  // Where webpack outputs the assets and bundles
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+
+  // Customize the webpack build process
+  plugins: [
+    // Vue plugin for the magic
+    new VueLoaderPlugin(),
+
+    // Removes/cleans build folders and unused assets when rebuilding
+    new CleanWebpackPlugin(),
+
+    // Copies files from target to destination folder
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
+    // Generates an HTML file from a template
+    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      // favicon: paths.src + '/images/favicon.png',
+      template: paths.src + '/template.html', // template file
+      filename: 'index.html', // output file
+    }),
+  ],
+
+  // Determine how modules within the project are treated
+  module: {
+    rules: [
+      // JavaScript: Use Babel to transpile JavaScript files
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+
+      // Images: Copy image files to build folder
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      // Fonts and SVGs: Inline files
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+    ],
+  },
+
+  resolve: {
+    modules: [paths.src, 'node_modules'], //search through ../src and nod_modules
+    extensions: ['.js', '.Vue', '.jsx', '.json'],
+    alias: {
+      '@': paths.src, //uses this name insted of us doing ../src
+      assets: paths.public, //uses this name insted of us doing ../public
+      process: "process/browser"
+      // 'components': pathtoresolve.resolve(__dirname, '../src/components/'),
+      // 'images': pathtoresolve.resolve(__dirname, '../src/images/'),
+      // 'styles': pathtoresolve.resolve(__dirname, '../src/styles/'),
+    },
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>A very Simple vue template</h1>
+    <p>The template was built with webpack from the ground up</p>
+  </div>
+</template>
